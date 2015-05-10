50d40d7a63cde234417f7078427ccb57dd5c6e91
Ben Mather <brmather1@gmail.com>
2015-05-10 22:56:27 +1000

    Finally committing features to the TherMesh class with an accompanying geothermal iPython notebook.
    Noteable features are:
    - Solves del2 implicitly using gaussian elimination with variable
      diffusivity and heat-source terms.
    - Can set variable boundary conditions on a node-by-node basis
      or by a single value.
    - It's FAST!

--- thermesh/thermesh.py
+++ thermesh/thermesh.py
@@ -28,66 +28,92 @@ class ThermMesh(TreMesh):
         self.temperature = temperature
         self.diffusivity = diffusivity
         self.heat_sources = heat_sources
-            
+
+
+        size = len(points_x)
+        for nl in self.neighbourhood_array:
+            size += len(nl)
+
+        # Initialise matrices
+        self.row_array = np.zeros(size, dtype=int)
+        self.col_array = np.zeros(size, dtype=int)
+        self.val_array = np.zeros(size)
+        self.nPos = 0
+        self.mCO_vector = np.zeros(len(points_x))
+
+        self.BCs = dict()
+
+        return
+
+
+    def create_BC(self, name, value, indices, is_flux):
+        """ Create a new BC with a given set of indices """
+
+        if type(value) is float or int:
+            value = value*np.ones_like(indices, dtype=float)
+        elif len(value) == len(indices):
+            pass
+        else:
+            raise ValueError('Length of indices / values do not match')
+
+        self.BCs[str(name)] = {'value': value, 'indices': indices, 'is_flux': bool(is_flux)}
+        
+
+        if self.verbose:
+            if is_flux:
+                print " - Neumann boundary condition '%s' created" % name
+            else:
+                print " - Dirichlet boundary condition '%s' created" % name
+
         return
 
 
-    def boundary_conditions(self, topBC, bottomBC, leftBC=0.0, rightBC=0.0, fluxBC=False):
+    def boundary_conditions(self, topBC, bottomBC, leftBC=0.0, rightBC=0.0):
         """
-        Seperate boundary mask into quadrants to assign BCs
-        DO NOT use Lloyd's mesh improvement algorithm!
+        1) Seperate boundary mask into quadrants
+        2) Assign BC values (leftBC and rightBC are insulated by default)
         """
 
-        self.leftBC = leftBC
-        self.rightBC = rightBC
-        self.topBC = topBC
-        self.bottomBC = bottomBC
+        inverse_bmask = np.invert(self.bmask)
 
-        self.fluxBC = fluxBC
+        leftBC_coords = np.where(self.x == min(self.x[inverse_bmask]))[0]
+        rightBC_coords = np.where(self.x == max(self.x[inverse_bmask]))[0]
+        topBC_coords = np.where(self.y == max(self.y[inverse_bmask]))[0]
+        bottomBC_coords = np.where(self.y == min(self.y[inverse_bmask]))[0]
 
-        leftBC_mask = np.zeros_like(self.bmask, dtype=bool)
-        rightBC_mask = np.zeros_like(self.bmask, dtype=bool)
-        topBC_mask = np.zeros_like(self.bmask, dtype=bool)
-        bottomBC_mask = np.zeros_like(self.bmask, dtype=bool)
+        # Remove duplicates
+        leftBC_coords = np.delete(leftBC_coords, [i for i,item in enumerate(leftBC_coords) if item in bottomBC_coords])
+        leftBC_coords = np.delete(leftBC_coords, [i for i,item in enumerate(leftBC_coords) if item in topBC_coords])
+        rightBC_coords = np.delete(rightBC_coords, [i for i,item in enumerate(rightBC_coords) if item in bottomBC_coords])
+        rightBC_coords = np.delete(rightBC_coords, [i for i,item in enumerate(rightBC_coords) if item in topBC_coords])
 
-        leftBC_mask[np.where(self.x == min(self.x))] = True
-        rightBC_mask[np.where(self.x == max(self.x))] = True
-        topBC_mask[np.where(self.y == max(self.y))] = True
-        bottomBC_mask[np.where(self.y == min(self.y))] = True
+        flux = np.zeros(4, dtype=bool)
+        # Flux conditions in W/m^2 -- should be < 1
+        for i, bc in enumerate([leftBC, rightBC, topBC, bottomBC]):
+            if abs(bc) < 1:
+                flux[i] = True
 
-        # check to make sure we haven't got too many elements
-        # if np.sum(self.bmask) != np.sum(topBC_mask)+np.sum(bottomBC_mask)+np.sum(leftBC_mask)+np.sum(rightBC_mask):
-            # print "boundary mask is irregular"
-
-        self.leftBC_mask = leftBC_mask
-        self.rightBC_mask = rightBC_mask
-        self.topBC_mask = topBC_mask
-        self.bottomBC_mask = bottomBC_mask
+        self.create_BC('leftBC', leftBC, leftBC_coords, is_flux=flux[0])
+        self.create_BC('rightBC', rightBC, rightBC_coords, is_flux=flux[1])
+        self.create_BC('topBC', topBC, topBC_coords, is_flux=flux[2])
+        self.create_BC('bottomBC', bottomBC, bottomBC_coords, is_flux=flux[3])
 
         return
+
 
     def initial_conditions(self):
         """
         Create an initial temperature field using an average gradient
+        - This is useful for temperature-dependent solves
         """
         temperature = np.zeros_like(self.temperature)
 
-        for row in range(len(self.neighbourhood_array)):
+        for row in range(len(self.x)):
             temperature[row,:] = self.topBC + gradient*dy
 
         self.temperature = temperature
 
-        return
-
-    def temperature_dependent(self):
-        """
-        Apply an operation to the conductivity field using a user-defined function
-        """
-        self.temperature_dependent = True
-
-        # conductivity = self.conductivity * (298./self.temperature)**
-
-        return
+        return temperature
 
 
     def delaunay_diffusion_rate(self, kappa, PHI):
@@ -125,64 +151,79 @@ class ThermMesh(TreMesh):
         """
         t = time.clock()
 
-        def writeMatrix(Ir, Jr, Vr):
-            """ Writes nodes to a sparse tridiagonal matrix """
-            global row_array, col_array, val_array, nPos
-            row_array[nPos] = Ir
-            col_array[nPos] = Jr
-            val_array[nPos] = Vr
-            nPos += 1
+        self.nPos = 0
+        index = np.arange(len(self.x), dtype=int)
 
-        size = 0
-        for nl in self.neighbourhood_array:
-            size += len(nl)
+        ## Boundary conditions
+        for key, attributes in self.BCs.iteritems():
+            idx = 0
+            if attributes['is_flux'] is True:
+                for row in index[attributes['indices']]:
+                    neighbours = self.neighbourhood_array[row]
+                    cneighbours = self.closed_neighbourhood_array[row]
+                    npoints = self.tri.points[neighbours]
+                    cnpoints = self.tri.points[cneighbours]
+                    nkappa = self.diffusivity[neighbours]
+                    
+                    delta, ad = np.empty(len(neighbours)), np.empty(len(neighbours))
+                    for col, column in enumerate(neighbours):
+                        coord = ( npoints[col-1] - cnpoints[col+1] )
+                        delta[col] = math.hypot(coord[0], coord[1])
+                        kappa = ( nkappa[col] + self.diffusivity[row] )
+                        ad[col] = kappa / (2*delta[col]**2)
+                        
+                        self._writeMatrix(row, column, 1.0*ad[col])
+                        
+                    self._writeMatrix(row, row, np.sum(ad*-1))
+                    self.mCO_vector[row] = attributes['value'][idx]/np.sum(delta) - self.heat_sources[row]
+                    idx += 1
 
-        global row_array, col_array, val_array, nPos
-        row_array = np.empty(size*5, dtype=int)
-        col_array = np.empty(size*5, dtype=int)
-        val_array = np.zeros(size*5)
-        nPos = 0
-        mCO_vector = np.zeros(size*5)
+            else:
+                for row in index[attributes['indices']]:
+                    self._writeMatrix(row, row, 1.0)
+                    self.mCO_vector[row] = attributes['value'][idx]
+                    idx += 1
 
-        idx=0    
-        for row in range(0, len(self.neighbourhood_array)):    
-            neighbours  = self.neighbourhood_array[row]  
-            cneighbours = self.closed_neighbourhood_array[row] 
-            npoints  = self.tri.points[neighbours]
+
+
+        ## Main body
+        for row in index[self.bmask]:
+            neighbours = self.neighbourhood_array[row]
+            cneighbours = self.closed_neighbourhood_array[row]
+            npoints = self.tri.points[neighbours]
             cnpoints = self.tri.points[cneighbours]
-
-            for col, column in enumerate(neighbours):  
-
-                Di = [self.diffusivity[row-1,column], self.diffusivity[row,column], self.diffusivity[row+1,column]]
-                Dj = [self.diffusivity[row,column-1], self.diffusivity[row,column], self.diffusivity[row,column+1]]
-
-                coord1m  =  npoints[col-1]
-                coord1p  = cnpoints[col+1]
-
-                delta =   (coord1p - coord1m)
-                adx = 1.0 / (2*delta[1]**2)
-                ady = 1.0 / (2*delta[0]**2)
-
-                # row_array[idx] = row
-                # col_array[idx] = column
-                # val_array[idx] =  delta[1] * self.weight[column]
-                # mCO_vector[idx] = -delta[0] * self.weight[column]
-
-                writeMatrix(index[idx], index[idx]-nx, (Dj[0]+Dj[1])*ady)
-                writeMatrix(index[idx], index[idx]-1, (Di[0]+Di[1])*adx)
-                writeMatrix(index[idx], index[idx], (Di[0]+2*Di[1]+Di[2])*-adx + (Dj[0]+2*Dj[1]+Dj[2])*-ady)
-                writeMatrix(index[idx], index[idx]+1, (Di[2]+Di[1])*adx)
-                writeMatrix(index[idx], index[idx]+nx, (Dj[2]+Dj[1])*ady)
-
-                matC[index[idx]] = -heatProduction[r,c]
-
-                idx += 1
+            nkappa = self.diffusivity[neighbours]
+            
+            ad = np.empty(len(neighbours))
+            for col, column in enumerate(neighbours):
+                coord = ( npoints[col-1] - cnpoints[col+1] )
+                delta = math.hypot(coord[0], coord[1])
+                kappa = ( nkappa[col] + self.diffusivity[row] )
+                ad[col] = kappa / (2*delta**2)
+                
+                self._writeMatrix(row, column, 1.0*ad[col])
+                
+            self._writeMatrix(row, row, np.sum(ad*-1))
+            self.mCO_vector[row] = -self.heat_sources[row]
 
 
-        A = sparse.coo_matrix( (val_array, (row_array, col_array)) ).tocsr()
-        b = sparse.coo_matrix( (mCO_vector), shape=(row_array, 1) ).T
+        A = sparse.coo_matrix( (self.val_array, (self.row_array, self.col_array)) ).tocsr()
+        b = sparse.coo_matrix( (np.array(self.mCO_vector) ), shape=(len(self.x), 1) ).T
 
         temperature = sparse.linalg.spsolve(A, b.tocsr())
-        # self.temperature = temperature
+        self.temperature = temperature
+
+
+        if self.verbose:
+            print " - Solving implicit conduction", (time.clock() - t), "s"
 
         return temperature
+
+
+    def _writeMatrix(self, Ir, Jr, Vr):
+        """ Writes nodes to a sparse tridiagonal matrix """
+        self.row_array[self.nPos] = Ir
+        self.col_array[self.nPos] = Jr
+        self.val_array[self.nPos] = Vr
+        self.nPos += 1
+        return
