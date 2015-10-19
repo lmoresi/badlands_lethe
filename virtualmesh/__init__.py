75099ad5ee16217fff3bc37d394589baffd63b78
Louis Moresi <louis.moresi@unimelb.edu.au>
2015-10-20 04:13:55 +1100

    More re-organising. Code not working yet !

--- virtualmesh/__init__.py
+++ virtualmesh/__init__.py
@@ -1,19 +1,21 @@
 """
-Module for generating and manipulating unstructured 2D meshes for surface process simulations
+Module for generating and manipulating  2D meshes for surface process simulations
 
 The module defines a meshing class and some tools to create simple standalone meshes and calculate derivatives etc
 
-    TreMesh
+    TriMesh
+    PixMesh
+    HeightMesh
+    SurfaceProcessMesh
 
-The TreMesh class builds a Delaunay triangulation and some higher-level data structures for 
+The TriMesh class builds a Delaunay triangulation and some higher-level data structures for
 consistent neighbourhood sweeping and differential operators (grad, div, del-squared) from
 the triangulation.
 
-Some plotting functions are included. 
+Some plotting functions are included.
 
 See the help for each class / function for more detailed information
 
 """
 
-from .treMesh     import TreMesh
-from .meshshape   import Shape2Mesh
+import class_definitions
