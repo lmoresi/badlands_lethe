a0ba208d80d3937623bdbdeae3aad9eab7050e3b
Ben Mather (desktop) <brmather1@gmail.com>
2016-03-10 15:54:38 +1100

    solver function accepts keyword arguments to pass to the PETSc KSP object. You can now specify tolerance, max iterations etc. KSP and Preconditioner methods should work better now.

--- petsc/solver.py
+++ petsc/solver.py
@@ -3,7 +3,7 @@ import sys, petsc4py
 from petsc4py import PETSc
 petsc4py.init(sys.argv)
 
-def solve(A, b, ksp=None, pc=None, comm=None):
+def solve(A, b, ksp=None, pc=None, comm=None, **kwargs):
     """
     Solve the system of equations Ax = b for x.
 
@@ -12,8 +12,11 @@ def solve(A, b, ksp=None, pc=None, comm=None):
             b       : right hand size PETSc vector object
 
         OPTIONAL ARGUMENTS
-            pc      : Preconditioner
-            ksp     : Krylov space method
+            pc      : Preconditioner (str or Preconditioner object)
+            ksp     : Krylov space method (str or KSP object)
+                      -- if None uses PETSc default: 'gmres'
+            kwargs  : Optional keyword arguments to be passed to KSP
+                      e.g. rtol=10e-5, atol=10e-50, divtol=10e5, mat_it=10e4
 
         RETURNS
             x       : PETSc vector object
@@ -23,13 +26,17 @@ def solve(A, b, ksp=None, pc=None, comm=None):
 
     KSP = PETSc.KSP()
     KSP.create(comm=comm)
-    if type(pc) == str:
+    if isinstance(pc, str):
         KSP.getPC().setType(pc)
     elif pc != None:
         KSP.setPC(pc)
 
-    if pc != None:
+    if ksp != None:
         KSP.setType(ksp)
+
+    for key, value in kwargs.items():
+        setattr(KSP, key, value)
+
     KSP.setFromOptions()
 
     KSP.setOperators(A.mat)
@@ -42,8 +49,11 @@ def solve(A, b, ksp=None, pc=None, comm=None):
 
 class Solver(PETSc.KSP):
     """
-    Useful if you want to solve multiple SLEs using the same matrix.
-    Preconditioning can vastly reduce solving time.
+    Useful for solving successive linear systems using the same matrix.
+    Use the `solve` function if preconditioning is not required, there is
+    no performance advantage using this object without preconditioning.
+
+    Preconditioning the left matrix can vastly reduce solving time.
     Common preconditioners are:
 
         PARALLEL PC TYPES
@@ -59,19 +69,31 @@ class Solver(PETSc.KSP):
     """
 
     def __init__(self, ksp='gmres', pc=None, comm=None):
+        """
+        Preconditioning the left matrix is mandatory.
+
+            ARGUMENTS
+                pc        : Preconditioner (str or Preconditioner object)
+                ksp       : Krylov space method (str or KSP object)
+                comm      : MPI communicator object
+                            -- use mpi4py.MPI.COMM_WORLD
+        """
 
         # Construct solver
-        PETSc.KSP.__init__(self)
+        super(Solver, self).__init__()
         self.create(comm=comm)
         self.getPC().setType(pc)
         self.setType(ksp)
         self.setFromOptions()
+        # Use previous solution vector to help solve
+        self.setInitialGuessNonzero(True)
 
 
     def solve(self, A, b):
         """
-        Solve a system of equations Ax = b to find x
+        Solve a system of equations Ax = b for x
         A and b must be PETSc objects.
+
             ARGUMENTS
                 A  : PETSc sparse matrix shape (m,m)
                 b  : PETSc vector (m,)
